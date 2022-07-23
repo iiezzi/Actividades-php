c2d087783306367010f12e73923a0c7036342af2
iiezzi <iezzi.2011@hotmail.com>
2022-07-23 09:23:12 -0300

    modificacion

--- sistema_ventas/tipoproducto-formulario.php
+++ sistema_ventas/tipoproducto-formulario.php
@@ -35,7 +35,7 @@ include_once("header.php");
           <h1 class="h3 mb-4 text-gray-800">Tipo de productos</h1>
            <div class="row">
                 <div class="col-12 mb-3">
-                    <a href="tipoproductos.php" class="btn btn-primary mr-2">Listado</a>
+                    <a href="tipoproducto-listado.php" class="btn btn-primary mr-2">Listado</a>
                     <a href="tipoproducto-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
                     <button type="submit" class="btn btn-success mr-2" id="btnGuardar" name="btnGuardar">Guardar</button>
                     <button type="submit" class="btn btn-danger" id="btnBorrar" name="btnBorrar">Borrar</button>
